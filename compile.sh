892f595eb527a6048e1b5c66d7ea10dec151df6c
voxel <voxel@dell.andreev.msk.ru>
2017-11-14 11:27:02 +0300

    Upgrade OpenSSL to 1.0.2m

--- compile.sh
+++ compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+export TZ=UTC
+
+# make tools/install
+# make -j4 toolchain/install
+# make -j4 target/compile
+PATH=/home/voxel/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 make V=99 >& report
 cd build_dir/target-arm_uClibc-0.9.33.2_eabi
-tar xmf ../../asm-openssl-1.0.2l.tar
+tar xmf ../../asm-openssl-1.0.2m.tar
 cd ../../
 make V=99 >& report1
