720a305728a5cdca4bee62c05115b4fc6d70a686
voxel <voxel@dell.andreev.msk.ru>
2018-12-14 09:44:19 +0300

    Upgrade OpenSSL to 1.0.2q and make an order in compilation

--- compile.sh
+++ compile.sh
@@ -7,7 +7,3 @@ export TZ=UTC
 # make -j4 target/compile
 PATH=/home/voxel/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 make V=99 >& report
-cd build_dir/target-arm_uClibc-0.9.33.2_eabi
-tar xmf ../../asm-openssl-1.0.2p.tar
-cd ../../
-make V=99 >& report1
