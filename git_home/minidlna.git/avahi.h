fe43a692956984ab760802ae335aee20bf46a521
voxel <voxel@dell.andreev.msk.ru>
2019-10-09 10:28:35 +0300

    Upgrade minidlna (1.2.1->2018-04-10); patches

--- git_home/minidlna.git/avahi.h
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-#if defined(TIVO_SUPPORT) && defined(HAVE_AVAHI)
-void tivo_bonjour_register(void);
-void tivo_bonjour_unregister(void);
-#else
-static inline void tivo_bonjour_register(void) {};
-static inline void tivo_bonjour_unregister(void) {};
-#endif
